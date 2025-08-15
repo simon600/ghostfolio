b2187706f021ba9e8009c781215be8d57b681c35
Szymon Fogiel <szymek.fogiel@gmail.com>
2025-08-15 10:42:21 +0000

    default host

--- libs/common/src/lib/config.ts
+++ libs/common/src/lib/config.ts
@@ -69,6 +69,7 @@ export const PORTFOLIO_SNAPSHOT_COMPUTATION_QUEUE_PRIORITY_LOW =
 
 export const DEFAULT_CURRENCY = 'USD';
 export const DEFAULT_DATE_FORMAT_MONTH_YEAR = 'MMM yyyy';
+export const DEFAULT_HOST = '0.0.0.0';
 export const DEFAULT_LANGUAGE_CODE = 'en';
 export const DEFAULT_PAGE_SIZE = 50;
 export const DEFAULT_PORT = 3333;
